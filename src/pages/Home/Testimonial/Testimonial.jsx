590d7c89912b32cc17ab35852191cec0295995a8
nurislam243 <mdnurislam15177@gmail.com>
2025-08-28 09:25:19 +0600

    added Testimonial section

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,11 +1,72 @@
 import React from 'react';
+import profile1 from "../../../../src/assets/Testimonial/profile-1.jpg";
+import profile2 from "../../../../src/assets/Testimonial/profile-2.png";
+import testimonialBg from "../../../../src/assets/Testimonial/testimonial-bg.png";
+
 
 const Testimonial = () => {
-    return (
-        <div>
-            <h1>Testimonial Section</h1>
+  const testimonials = [
+  {
+      name: "Afiya Jaman Snigdha",
+      role: "UI/UX Designer",
+      testimonialText: "Explore our collection of carefully chosen products expert advice and inspiring stories designed to  help you discover and your full potential. Whether you are seeking inspiration advice or the perfect product to elevate your routine.",
+      imageUrl: profile1
+    },
+    {
+      name: "Rafiq Hasan Mahabuba",
+      role: "Frontend Developer",
+      testimonialText: "The product selection and customer support are excellent. I found exactly what I needed for my requirements. Their service is professional, timely, and highly recommended. I am very impressed with the quality, attention to.",
+      imageUrl: profile2
+    }
+
+  ];
+
+  return (
+    <section className="bg-base-100 py-20 flex flex-col items-center">
+        <h2 className="text-4xl font-bold text-center mb-16 text-base-content">Customer Testimonial</h2>
+
+        <div className="flex">
+            <div className="flex flex-col lg:flex-row items-center gap-12 lg:gap-8 w-full z-50">
+                {testimonials.map((testimonial, index) => (
+                <div
+                    key={index}
+                    className={`
+                     rounded w-full md:max-w-[424px] min-h-[284px] bg-base-100 shadow-md py-[48px] px-[30px]`
+                    }
+                >
+                    <div className="flex items-start space-x-4 mb-4 z-10">
+                    {/* Avatar */}
+                    <div className="avatar">
+                        <div className="w-12 rounded-full">
+                        <img src={testimonial.imageUrl} alt={`${testimonial.name}'s avatar`} />
+                        </div>
+                    </div>
+                    {/* Name and Role */}
+                    <div className="flex flex-col">
+                        <span className="font-bold text-lg text-primary">{testimonial.name}</span>
+                        <span className="text-sm text-base-content/60">{testimonial.role}</span>
+                    </div>
+                    </div>
+                    {/* Testimonial Text */}
+                    <p className="text-base-content text-sm md:text-base leading-relaxed z-10">
+                    {testimonial.testimonialText}
+                    </p>
+                </div>
+                ))}
+            </div>
+              
+            {/* Image */}
+            <div className="relative hidden -left-[106px] lg:block">
+                <img
+                    src={testimonialBg}
+                    alt="Product"
+                    className="min-w-[536px] h-full top-0 shadow-2xl"
+                />
+            </div>
         </div>
-    );
+
+    </section>
+  );
 };
 
 export default Testimonial;
